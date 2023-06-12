aec461dc6f348f3451ff0e7e3c3a12fb096de0f5
Pandra14 <130453008+Pandra14@users.noreply.github.com>
2023-06-12 09:52:01 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+let conductividad = 0
+input.onButtonPressed(Button.A, function () {
+    conductividad = pins.analogReadPin(AnalogPin.P0)
+    basic.showNumber(conductividad)
+    radio.sendNumber(conductividad)
+})
 basic.forever(function () {
-	
+    radio.setGroup(1973)
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        led.setBrightness(255)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        led.setBrightness(0)
+    }
 })
